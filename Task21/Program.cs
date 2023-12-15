70e5ade8d1c30801d09260d2074f0aee5d4c109b
unknown <igoshin1922@gmail.com>
2023-12-15 22:12:22 +0500

    added task21

---
+++ Task21/Program.cs
@@ -0,0 +1,14 @@
+﻿
+void GetResult(string txt) {
+    string[] nums = txt.Split(',');
+    int summ = 0;
+        for(int i = 0; i < nums.Count(); i ++)
+        {
+        summ += int.Parse(nums[i]);
+        }
+    Console.WriteLine($"summ = {summ}");
+}
+
+Console.WriteLine("Введите строку чисел ( числа отделяются друг от друга запятой ) : ");
+string text = Console.ReadLine();
+GetResult(text);
